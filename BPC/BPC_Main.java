722447729e7a9dd368dd4c0d52faba77432ed820
Muhammad Nouman Shafiq <ms24aga@herts.ac.uk>
2025-04-13 22:12:37 +0100

    First Functionality Added: Add/Remove Patient

--- BPC/BPC_Main.java
+++ BPC/BPC_Main.java
@@ -2,52 +2,89 @@ package BPC;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BPC_Main {
-    public List<Physiotherapist> physiotherapists=new ArrayList<>();
+    static int nextPatientId = 1;
+    public List<Physiotherapist> physiotherapists = new ArrayList<>();
+    private List<Patient> patients = new ArrayList<>();
 
-    public void addPhysiotherapist(Physiotherapist physiotherapist) {
+    /*public void addPhysiotherapist(Physiotherapist physiotherapist) {
         physiotherapists.add(physiotherapist);
     }
+
     public void printPhysiotherapists() {
         for (Physiotherapist physiotherapist : physiotherapists) {
             System.out.println(physiotherapist);
         }
+    }*/
+
+    public boolean isPatientIdExists(int id) {
+        for (Patient patient : patients) {
+            if (patient.getId() == id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public void addPatient(Patient patient) {
+        patients.add(patient);
+    }
+
+
+    public boolean removePatient(int id) {
+        for (Patient patient : patients) {
+            if (patient.getId() == id) {
+                patients.remove(patient);
+                System.out.print("Patient removed from the clinic");
+                return true;
+            }
+            else {
+                System.out.print("Patient not found");
+                return false;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         BPC_Main main = new BPC_Main();
-        Patient patient = new Patient(1, "Nouman","156 Baker St", 12311241);
 
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Before appointments:");
-        System.out.println(patient);
-
-        patient.addAppointmentId(101);
-
-        System.out.println("After adding appointments:");
-        System.out.println(patient);
-// Physiotherapists
-
-
-        Physiotherapist helen = new Physiotherapist(1, "Helen", "123 Main St", 5551234);
-        helen.addExpertise("Physiotherapy");
-        helen.addExpertise("Massage");
-
-        Physiotherapist john = new Physiotherapist(2, "John", "456 Elm St", 555567);
-        john.addExpertise("Rehabilitation");
-        john.addExpertise("Acupuncture");
-
-        Physiotherapist sara = new Physiotherapist(3, "Sara", "789 Maple St", 5554321);
-        sara.addExpertise("Osteopathy");
-        sara.addExpertise("Pool Rehabilitation");
-
-        main.addPhysiotherapist(helen);
-        main.addPhysiotherapist(john);
-        main.addPhysiotherapist(sara);
-
-        System.out.println("Physiotherapists in Clinic:");
-        main.printPhysiotherapists();
-
+        while (true) {
+            System.out.println("\n--- Boost Physio Clinic ---");
+            System.out.println("1. Add Patient");
+            System.out.println("2. Remove Patient");
+            System.out.print("Enter your choice: ");
+            int subChoice=scanner.nextInt();
+            scanner.nextLine();
+            switch (subChoice) {
+                case 1:
+                    while (main.isPatientIdExists(nextPatientId)) {
+                        nextPatientId++;
+                    }
+                    int patientId = nextPatientId++;
+                    System.out.print("Enter patient name: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Enter patient address: ");
+                    String address = scanner.nextLine();
+                    System.out.print("Enter patient phone: ");
+                    int phone = scanner.nextInt();
+                    main.addPatient(new Patient(patientId, name, address, phone));
+                    System.out.print("Patient added with ID: " + patientId + " Name: " + name + " Address: " + address + " Phone: " + phone + "\n");
+                    System.out.print("\nKindly Remember your ID for future");
+                    break;
+                case 2:
+                    System.out.print("Enter patient ID to remove: ");
+                    patientId = scanner.nextInt();
+                    main.removePatient(patientId);
+                    scanner.nextLine();
+                    break;
+            }
+        }
     }
 }
+
